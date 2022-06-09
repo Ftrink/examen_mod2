99fa332c85e3a04a9d86efb7179df1f8cb20c8eb
Felipe Trincado <f.trincado7@gmail.com>
2022-06-09 13:35:20 -0400

    addToDom_agrega

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function randomWords (palabras){
-    return palabras[Math.floor(math.random()*palabras.length)]
-}
-
+// variables
+let palabraAleatoria;
+let time = 10;
+let score = 0;
+let hone = document.querySelector('#randomWord'); //h1
+let palabraingresada;
+// listado palabras
 const words = [
     'californication',
     'plataforma5',
@@ -22,5 +25,48 @@ const words = [
     'zephyr',
     'funky',
     'chili'
-    ];
-    console.log(randomWords)
+];
+
+//functions
+function randomWords (palabras){
+    return palabras[Math.floor(Math.random()*palabras.length)] 
+}
+function addToDOM (){
+    const palabraalazar = randomWords(words);
+    hone.textContent = palabraalazar
+} 
+
+// inicia el juego
+addToDOM()
+
+const inputt = document.querySelector('#inputt')
+inputt.addEventListener('keyup', function(evn){
+        if(evn.keyCode == 13){ 
+            palabraingresada = inputt.value;
+            for(let i of words)
+            if (i == palabraingresada ){
+                inputt.value = '';        // limpia el input
+                console.log('es igual')
+
+
+            /*Que la variable time aumente 3 segundos.
+            ● Que el input vuelva a quedar vacío.
+            
+            ● Volver a invocar la Función addToDOM()  AUTOMATICO CON ENTER ?   */            
+        }else{
+            console.log('Nooopee')
+        }
+    }
+    //console.log(evn)
+
+
+/* inputt.addEventListener('keyup' , function(evn){
+            const ingreso = inputt.value;
+            for(let indice of css_color_names){
+            if(indice == ingreso){
+                caja1.style['background-color'] = ingreso;
+                                return
+            }  
+        }*/
+
+})
